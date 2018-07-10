f15b6d5dfbc9773ef1520803b8292e8bb39c15b4
Alex <kenda969@gmail.com>
2018-07-11 01:20:26 +0300

    v4.8.1

--- js/filters.js
+++ js/filters.js
@@ -14,51 +14,39 @@
     filters.classList.remove('filters-inactive');
   }
   function filterComments(photoGallery) {
-    
-    var imageComments = photoGallery.map(function (comment){
-      return comment.comments;
-    });
-    var q;
-    for(var i = 0; i < imageComments.length; i++){
-
-       q = imageComments[i];
-       console.log(q);
-
-    }
-
-
-
-
-    // sortingUseComb(imageCommentsItem);
-
-
-    
-  }
-  // Фильтр (ПОПУЛЯРНЫЕ) функция возвращает новый массив отсортированный по лайкам(порода собаки ;))) ).
-  function filterPopular(photoGallery) {
-    var popularPhoto = [];
+    var discuss = [];
+    var discussPhoto;
     var obj;
-	  var imageLikes = photoGallery.map(function (likes) {
+    var PhotoComments = photoGallery.map(function (comments) {
+      return comments.comments;
+    });
+    for (var i = 0; i < PhotoComments.length; i++) {
+      discuss.push(obj = {
+        arr: PhotoComments[i].length
+      });
+    }
+    var commentsCountItems = discuss.map(function (arr) {
+     return arr.arr;
+    });
+    sortingUseComb(commentsCountItems);
+    discussPhoto = getNewPhotoGallery(photoGallery,commentsCountItems);
+    console.log(discussPhoto);
+    return discussPhoto;
+  }
+  // Фильтр (ПОПУЛЯРНЫЕ).
+  function filterPopular(photoGallery) {
+    var popularPhoto;
+	  var photoLikes = photoGallery.map(function (likes) {
 		  return likes.likes;
 	  });
 
-	  sortingUseComb(imageLikes);
+	  sortingUseComb(photoLikes);
+	  popularPhoto =  getNewPhotoGallery(photoGallery,photoLikes);
+	  console.log(popularPhoto);
+    return popularPhoto;
 
-      // создание нового массива.
-	    for(var i = 0; i < photoGallery.length; i++){
-	      for(var j = 0; j < photoGallery.length; j++){
-          if(imageLikes[0] === photoGallery[j].likes){
-            popularPhoto.push(obj = {
-              url: photoGallery[j].url,
-              likes: photoGallery[j].likes,
-              comments: photoGallery[j].comments
-            });
-            imageLikes.shift(i);
-          }
-        }
-      }
-      return popularPhoto;
   }
+
   // Сортировка расчесткой.
   function sortingUseComb(arr) {
 
@@ -76,6 +64,25 @@
 
 
     }
+  }
+
+  // создание нового массива.
+  function getNewPhotoGallery(photoGallery, arr) {
+    var obj;
+    var newPhotoGallery = [];
+    for(var i = 0; i < photoGallery.length; i++){
+      for(var j = 0; j < photoGallery.length; j++){
+        if(arr[0] === photoGallery[j].likes || arr[0] === photoGallery[j].comments.length){
+          newPhotoGallery.push(obj = {
+            url: photoGallery[j].url,
+            likes: photoGallery[j].likes,
+            comments: photoGallery[j].comments
+          });
+          arr.shift(i);
+        }
+      }
+    }
+      return newPhotoGallery;
   }
 
   window.filter = {
